81e0c833efa686e71aa40a957c531f792695667d
Michael <michaelrharding34@gmail.com>
2025-03-27 09:47:38 -0400

    add header

--- flaskery.py
+++ flaskery.py
@@ -1,9 +1,10 @@
-#------------TO RUN: in git bash terminal, type "flask --app finalproject run"----------------
+#------------TO RUN: in git bash terminal, type "flask --app {filename} run"----------------
 from flask import Flask as fl
 
 app = fl(__name__)
 
 @app.route("/")
 
-def hello_world():
-    return "<p> Hello, world! </p>"
+@app.route("/home")
+def home():
+    return "<h1> To-Do List </h1>"
