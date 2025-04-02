426f32c5f30ed9962125b4bd26359849ebf774e5
Michael <michaelrharding34@gmail.com>
2025-04-02 15:53:34 -0400

    add debug and clean up flask file

--- flaskery.py
+++ flaskery.py
@@ -1,10 +1,13 @@
 #------------TO RUN: in git bash terminal, type "flask --app {filename} run"----------------
-from flask import Flask as fl
+from flask import Flask
+from flask import render_template
 
-app = fl(__name__)
+app = Flask(__name__)
 
 @app.route("/")
-
 @app.route("/home")
 def home():
-    return "<h1> To-Do List </h1>"
+    return render_template("main.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
